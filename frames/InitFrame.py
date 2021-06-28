c0199cec2031b2ca03313ef5dc60c74f860af96f
Alberto Vazquez <albertovmrm@gmail.com>
2021-06-28 16:30:24 +0200

    Only one system check per execution

--- frames/InitFrame.py
+++ frames/InitFrame.py
@@ -59,73 +59,80 @@ class InitFrame:
             self.buttons_list[index].destroy()
             self.buttons_border_list[index].destroy()
 
+    def check_os(self, master):
+        
+        if ("Microsoft Windows Server 2012" in gui.g_dict_osinfo["OsName"] or 
+            "Microsoft Windows Server 2016" in gui.g_dict_osinfo["OsName"] or 
+            "Microsoft Windows Server 2019" in gui.g_dict_osinfo["OsName"]):
+
+            messagebox.showinfo("Check operating system","Version " + gui.g_dict_osinfo["OsName"] + " is valid")
+            # Delete current widgets
+            self.destroy_items()
+            # Create next frame menu
+            e = MainFrame.MainFrame(master)
+
+        else:
+            messagebox.showwarning("Check operating system", "Must be a Windows version 2012/2016/2019 " + "\n(You have " + gui.g_dict_osinfo["OsName"] + ")")
+   
     # Check if the system is Windows Server
     def check_system(self, master):
-        if platform.system() == 'Windows':
-            # Create background subprocess for cmdlet
-            p = subprocess.Popen(['powershell.exe', "-ExecutionPolicy", "unrestricted", '.\\scripts\\hashtable.ps1'], stdout=subprocess.PIPE, stderr= subprocess.PIPE)
-           
-            top = tk.Toplevel()
-            top.resizable(False, False)
-            top.geometry("400x80")
+        if len(gui.g_dict_osinfo) == 0:
+            if platform.system() == 'Windows':
+                # Create background subprocess for cmdlet
+                p = subprocess.Popen(['powershell.exe', "-ExecutionPolicy", "unrestricted", '.\\scripts\\hashtable.ps1'], stdout=subprocess.PIPE, stderr= subprocess.PIPE)
             
-            top.title("Checking OS information")
-            top.iconbitmap("img/icaad.ico")
-            lbl = tk.Label(top, text= "Executing command Get-ComputerInfo.", bg='white', fg='#707070')
-            lbl.pack(pady=10)
-            s = ttk.Style()
-            s.theme_use("winnative")
-            s.configure("blue.Horizontal.TProgressbar",troughcolor='#f2f2f2', foreground='white', background='#0077d1')
-            progress_bar = ttk.Progressbar(top, style="blue.Horizontal.TProgressbar", orient=tk.HORIZONTAL, length=300, mode="indeterminate")
-            progress_bar.pack()
-            
-            cnt = 0
-            
-            # Show progress bar while cmdlet is running
-            while (p.poll() is None):
+                top = tk.Toplevel()
+                top.resizable(False, False)
+                top.geometry("400x80")
                 
-                if progress_bar['value'] == 100:
-                    progress_bar['value'] = 0
-                else: 
-                    progress_bar['value'] += 5%100
-
-                    if progress_bar['value']%25 == 0:
-                        if cnt == 2:
-                            lbl['text'] = lbl['text'][:-2]
-                            cnt = 0
-                        else:
-                            lbl['text'] += "."
-                            cnt += 1
-
-                top.update_idletasks()
-                time.sleep(0.125)
-
-            top.destroy()
-
-            skip_title = 0
-            for line in p.stdout:
-                if skip_title == 3 and len(line) > 2:
-                    decoded_line = line.decode('utf-8', errors='strict').strip()
-                    clean_line = re.sub('\s+',' ', str(decoded_line))
+                top.title("Checking OS information")
+                top.iconbitmap("img/icaad.ico")
+                lbl = tk.Label(top, text= "Executing command Get-ComputerInfo.", bg='white', fg='#707070')
+                lbl.pack(pady=10)
+                s = ttk.Style()
+                s.theme_use("winnative")
+                s.configure("blue.Horizontal.TProgressbar",troughcolor='#f2f2f2', foreground='white', background='#0077d1')
+                progress_bar = ttk.Progressbar(top, style="blue.Horizontal.TProgressbar", orient=tk.HORIZONTAL, length=300, mode="indeterminate")
+                progress_bar.pack()
+                
+                cnt = 0
+                
+                # Show progress bar while cmdlet is running
+                while (p.poll() is None):
                     
-                    key = clean_line.split()[0]
-                    gui.g_dict_osinfo[key] = clean_line.partition(' ')[2]
+                    if progress_bar['value'] == 100:
+                        progress_bar['value'] = 0
+                    else: 
+                        progress_bar['value'] += 5%100
 
-                    if "Microsoft Windows" in str(line):
-                        win_ver = gui.g_dict_osinfo["OsName"]
-                else:
-                    skip_title += 1
+                        if progress_bar['value']%25 == 0:
+                            if cnt == 2:
+                                lbl['text'] = lbl['text'][:-2]
+                                cnt = 0
+                            else:
+                                lbl['text'] += "."
+                                cnt += 1
 
-            if "Microsoft Windows Server 2012" in win_ver or "Microsoft Windows Server 2016" in win_ver or "Microsoft Windows Server 2019" in win_ver:
-                messagebox.showinfo("Check operating system","Version " + win_ver + " is valid")
-               
-                # Delete current widgets
-                self.destroy_items()
-                
-                # Create next frame menu
-                e = MainFrame.MainFrame(master)
+                    top.update_idletasks()
+                    time.sleep(0.125)
+
+                top.destroy()
+
+                skip_title = 0
+                for line in p.stdout:
+                    if skip_title == 3 and len(line) > 2:
+                        decoded_line = line.decode('utf-8', errors='strict').strip()
+                        clean_line = re.sub('\s+',' ', str(decoded_line))
+                        
+                        key = clean_line.split()[0]
+                        gui.g_dict_osinfo[key] = clean_line.partition(' ')[2]
+
+                    else:
+                        skip_title += 1
+
+                self.check_os(master)
 
             else:
-                messagebox.showwarning("Check operating system", "Must be a Windows version 2012/2016/2019 " + "\n(You have " + win_ver + ")")
+                messagebox.showwarning("Check operating system", "Must be a Windows version 2012/2016/2019 " + "\n(You have " + gui.g_dict_osinfo["OsName"] + ")")
         else:
-            messagebox.showwarning("Check operating system", "Must be a Windows version 2012/2016/2019 " + "\n(You have " + platform.system() + " " + platform.release() + ")")
+            self.check_os(master)
