eb0c484ab0c7bd777112d82d98797f1db8fb9854
Alberto Vazquez <albertovmrm@gmail.com>
2022-06-11 16:00:08 +0200

    First aproach to config NIC menu

--- frames/ConfigNICFrame.py
+++ frames/ConfigNICFrame.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-import re
 from PIL import ImageTk,Image
-from tkinter import VERTICAL, ttk
+from tkinter import LEFT, ttk
 
 import gui
 from frames import InitFrame
@@ -11,19 +10,26 @@ class ConfigNICFrame:
         geometry = "500x600"
         master.geometry(geometry)
         res = geometry.split("x")
-        
+
+        self.list_entries1 = []
+        self.list_entries2 = []
+        self.list_entries3 = []
+        self.list_entries4 = []
+        self.list_all_entries = []
+        self.list_positions_entries = [0,0,0,0]
+
         self.frame = tk.Frame(master, bg='white', width=300, height=300)
         self.frame.pack(fill=tk.BOTH, expand=1)
 
         self.canvas = tk.Canvas(self.frame, bg='white', bd=0, highlightthickness=0)
         self.canvas.place(relx=0.5, rely=0.5, anchor=tk.CENTER, width=282, height=400)
 
-        self.scrollbar = ttk.Scrollbar(self.frame, orient=VERTICAL, command=self.canvas.yview)
+        self.scrollbar = ttk.Scrollbar(self.frame, orient=tk.VERTICAL, command=self.canvas.yview)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion= self.canvas.bbox("all")))
-        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+        self.canvas.bind_all("<MouseWheel>", self.on_mousewheel)
 
         self.second_frame = tk.Frame(self.canvas, bg='white')
         x0 = self.second_frame.winfo_screenwidth()/2
@@ -65,8 +71,65 @@ class ConfigNICFrame:
         self.buttons_border_list.append(self.cmd_button_border)
         self.buttons_list.append(self.cmd_button)
 
-    def _on_mousewheel(self, event):
-        self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+    # Event for mousewheel in scrollbar menus
+    def on_mousewheel(self, event):
+        try:
+            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        except:
+            pass
+
+    # Validate address entry format 
+    def validate(self, num_row, P):
+        if len(P) == 0:
+            return True
+        elif len(P) == 3 and P.isdigit():
+            for i in range(4):
+                if self.list_positions_entries[int(num_row)] == i and i != 3:
+                    self.list_all_entries[int(num_row)][i+1].focus()
+                    self.list_positions_entries[int(num_row)] = i+1
+                    break
+            return True
+        elif len(P) >= 1 and len(P) <= 3 and P.isdigit():
+            return True
+        else:
+            return False
+
+    # Define field to set addresses
+    def set_address_menu(self, frame, title, num_row, list_entries):
+        vcmd = (frame.register(self.validate), num_row, '%P')
+        
+        addr_label = tk.Label(frame, bg='white', text=title)
+        addr_label.grid(pady=3, padx=30, row=num_row, column=0, sticky=tk.W)
+
+        addr_entry1 = tk.Entry(frame, width= 5, bd=1.5, justify='center', validate="key", validatecommand=vcmd)
+        addr_entry1.grid(pady=3, row=num_row, column=1)
+
+        dot1_label = tk.Label(frame, bg='white', text=".")
+        dot1_label.grid(pady=3, row=num_row, column=2)
+
+        addr_entry2 = tk.Entry(frame, width= 5, bd=1.5, justify='center', validate="key", validatecommand=vcmd)
+        addr_entry2.grid(pady=3, row=num_row, column=3)
+
+        dot2_label = tk.Label(frame, bg='white', text=".")
+        dot2_label.grid(pady=3, row=num_row, column=4)
+
+        addr_entry3 = tk.Entry(frame, width= 5, bd=1.5, justify='center', validate="key", validatecommand=vcmd)
+        addr_entry3.grid(pady=3, row=num_row, column=5)
+        
+        dot3_label = tk.Label(frame, bg='white', text=".")
+        dot3_label.grid(pady=3, row=num_row, column=6)
+
+        addr_entry4 = tk.Entry(frame, width= 5, bd=1.5, justify='center', validate="key", validatecommand=vcmd)
+        addr_entry4.grid(pady=3, row=num_row, column=7)
+
+        list_entries.append(addr_entry1)
+        list_entries.append(addr_entry2)
+        list_entries.append(addr_entry3)
+        list_entries.append(addr_entry4)
+        list_entries[0].focus()
+    
+    def prueba(self):
+        print(str(self.list_entries1[0].get()) + "." + str(self.list_entries1[1].get()) + "." + str(self.list_entries1[2].get()) + "." + str(self.list_entries1[3].get()))
 
     # Display frame for NIC configuration
     def config_nic(self, master, id, res):
@@ -75,17 +138,17 @@ class ConfigNICFrame:
         frame = tk.Frame(master, bg='white', width=300, height=300)
         frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-        title_label = tk.Label(frame, font=("Helvetica", 15), bg='white', text= "Configuration of " +  gui.g_list_nic[id] + " NIC")
-        title_label.pack()
+        #title_label = tk.Label(frame, bg='white', text= "Configuration of " +  gui.g_list_nic[id] + " NIC")
+        #title_label.grid(pady=5, row=0, column=0)
 
-        ip_addr_label = tk.Label(frame, font=("Helvetica", 15), bg='white', text="IP Address")
-        ip_addr_label.pack()
+        self.set_address_menu(frame, "IP address", 0, self.list_entries1)
+        self.set_address_menu(frame, "Subnet mask", 1, self.list_entries2)
+        self.set_address_menu(frame, "Default gateway", 2, self.list_entries3)
+        self.set_address_menu(frame, "DNS server", 3, self.list_entries4)
+        self.list_all_entries = [self.list_entries1, self.list_entries2, self.list_entries3, self.list_entries4]
 
-        ip_addr_entry = tk.Entry(frame)
-        ip_addr_entry.pack()
-
-        button = tk.Button(frame, text="button", command = lambda: print(ip_addr_entry.get()))
-        button.pack()
+        button = tk.Button(frame, text="OK", command = lambda: self.prueba())
+        button.grid(pady=5, row=5, column=0)
 
         image2 = Image.open("img/back.png")
         image2 = image2.resize((30, 30), Image.ANTIALIAS)
@@ -106,6 +169,7 @@ class ConfigNICFrame:
         self.buttons_border_list.append(self.cmd_button_border)
         self.buttons_list.append(self.cmd_button)
 
+    # Destroy all items of this frame
     def destroy_all_items(self):
         gui.destroy_items(self.buttons_list, self.buttons_border_list)
         self.canvas.destroy()
@@ -114,6 +178,7 @@ class ConfigNICFrame:
         self.frame.destroy()
         self.icon_label.destroy()
 
+    # Back to config nic menu
     def go_back_config_nic(self, master):
         self.destroy_all_items()
         e = ConfigNICFrame(master)
